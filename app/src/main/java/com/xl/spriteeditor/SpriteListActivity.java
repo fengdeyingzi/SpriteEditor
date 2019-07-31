1c965606bcbc0d8d8db38296e7091ca20f09ac5e
yzjlb <2541012655@qq.com>
2019-07-31 17:58:57 +0800

    ...

--- app/src/main/java/com/xl/spriteeditor/SpriteListActivity.java
+++ app/src/main/java/com/xl/spriteeditor/SpriteListActivity.java
@@ -1,29 +1,87 @@
 package com.xl.spriteeditor;
 
+import android.Manifest;
 import android.os.Bundle;
+import android.widget.SeekBar;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.github.dfqin.grantor.PermissionListener;
 import com.github.dfqin.grantor.PermissionsUtil;
 import com.xl.spriteeditor.view.TestView;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-
 /**
  * 精灵列表界面
  */
-public class SpriteListActivity extends BaseActivity{
+public class SpriteListActivity extends BaseActivity {
+
+    private TestView textview;
+    private SeekBar seek_origin_x;
+    private SeekBar seek_origin_y;
+    private SeekBar seek_zoom;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sprlist);
 
-        setContentView(new TestView(this));
+        setContentView(R.layout.activity_test);
+        initView();
+seek_origin_x.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        textview.serOriginX(progress);
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
 
     }
 
-    public void requestPermission(){
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
+    }
+});
+
+
+seek_origin_y.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        textview.setOriginY(progress);
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
+    }
+});
+seek_zoom.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        textview.setZoom(progress/100f);
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
+    }
+});
+
+    }
+
+    public void requestPermission() {
         PermissionsUtil.requestPermission(getApplication(), new PermissionListener() {
             @Override
             public void permissionGranted(@NonNull String[] permissions) {
@@ -34,7 +92,7 @@ public class SpriteListActivity extends BaseActivity{
             public void permissionDenied(@NonNull String[] permissions) {
 
             }
-        }, android.Manifest.permission.READ_EXTERNAL_STORAGE, android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE);
     }
 
     @Override
@@ -51,5 +109,12 @@ public class SpriteListActivity extends BaseActivity{
     @Override
     public void onBackPressed() {
         super.onBackPressed();
+    }
+
+    private void initView() {
+        textview = (TestView) findViewById(R.id.textview);
+        seek_origin_x = (SeekBar) findViewById(R.id.seek_origin_x);
+        seek_origin_y = (SeekBar) findViewById(R.id.seek_origin_y);
+        seek_zoom = findViewById(R.id.seek_zoom);
     }
 }
--- app/src/main/java/com/xl/spriteeditor/view/TestView.java
+++ app/src/main/java/com/xl/spriteeditor/view/TestView.java
@@ -9,17 +9,20 @@ import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffColorFilter;
+import android.util.AttributeSet;
 import android.view.View;
+
+import androidx.annotation.Nullable;
 
 import com.xl.game.tool.XL;
 
 public class TestView extends View {
     Bitmap bitmap;
-    int origin_x=70,origin_y=70;
-    int x=0,y=180;
-    float zoom=3f;
+    int origin_x=0,origin_y=0;
+    int x=100,y=100;
+    float zoom=3.0f;
     int rotate=30;
-    int alpha=250;
+    int alpha=190;
     int color=0x90f090;
 
     public TestView(Context context){
@@ -27,9 +30,30 @@ public class TestView extends View {
         this.bitmap = XL.getImageFromAssetsFile(context, "sprite_icon.png");
     }
 
+    public TestView(Context context, @Nullable AttributeSet attrs){
+        super(context,attrs);
+        this.bitmap = XL.getImageFromAssetsFile(context, "sprite_icon.png");
+    }
+
+    public void serOriginX(int x){
+        origin_x = x;
+        invalidate();
+    }
+
+    public void setOriginY(int y){
+        origin_y = y;
+        invalidate();
+
+    }
+
+    public void setZoom(float zoom){
+        this.zoom = zoom;
+        invalidate();
+    }
+
 
     /**
-     *
+     * 实现图片的旋转 缩放 翻转 设置透明度绘制
      * @param bitmap     图片
      * @param origin_x  旋转 缩放 原点x
      * @param origin_y  旋转 缩放原点 y
@@ -41,26 +65,19 @@ public class TestView extends View {
      * @param alpha     不透明度
      * @param color     颜色
      */
-    public static void drawBitmap(Canvas canvas,Bitmap bitmap, int origin_x, int origin_y, int x, int y, float zoom,int flag, int rotate, int alpha, int color){
+    public static void drawBitmap(Canvas canvas,Bitmap bitmap, float origin_x, float origin_y, float x, float y, float zoom,int flag, float rotate, int alpha, int color){
         Matrix matrix = new Matrix();
         Paint paint = new Paint();
-
-        // 旋转
-        matrix.postRotate(rotate,x+origin_x,y+origin_y);
-
-        //设置透明度
-        paint.setAlpha(alpha);
-
-        //设置原点
-
-        // 缩放
-        matrix.postScale(zoom, zoom);
-
         if (flag == 1) //左右翻转
-            matrix.setScale(-1, 1);
+            matrix.setScale(-1, 1,origin_x,origin_y);
         if (flag == 2)  //上下翻转
-            matrix.setScale(1, -1);
+            matrix.setScale(1, -1,origin_x,origin_y);
+        // 缩放
+        matrix.postScale(zoom, zoom,origin_x,origin_y);
+        // 旋转
+        matrix.postRotate(rotate,origin_x,origin_y);
 
+        matrix.postTranslate(x,y);
         //颜色渲染
         /*
         if(color!=0){
@@ -90,17 +107,28 @@ public class TestView extends View {
 //            原文：https://blog.csdn.net/harvic880925/article/details/51253944
 //            版权声明：本文为博主原创文章，转载请附上博文链接！
         }
+        else{
+            //设置透明度
+            paint.setAlpha(alpha);
+        }
 
         canvas.drawBitmap(bitmap,matrix,paint);
     }
 
+    //绘制一个十字
+    private void drawLines(Canvas canvas,int x,int y){
+        Paint paint = new Paint();
+        canvas.drawLine(x,0,x,getHeight(),paint);
+        canvas.drawLine(0,y,getWidth(),y,paint);
+    }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
 
-canvas.drawBitmap(bitmap,300,0,null);
-        drawBitmap(canvas, bitmap,origin_x,origin_y,x,y,zoom,0,rotate,alpha,color);
+canvas.drawBitmap(bitmap,0,0,null);
+        drawBitmap(canvas, bitmap,origin_x,origin_y,x,y,zoom,1,rotate,alpha,color);
+        drawLines(canvas,x,y);
     }
 }
